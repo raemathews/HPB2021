e8c642af549d8727bfe7f701afb98f62a872a6d7
raemathews <mathews.ra@northeastern.edu>
2021-02-20 12:41:09 -0500

    switch between components

--- vaccine-tracker-frontend/src/App.js
+++ vaccine-tracker-frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Dashboard from './Dashboard.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import FormEntry from './FormEntry.js'
 
@@ -9,8 +9,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavLink activeClassName="active"
+          exact to={'/FormEntry'} >
+            switch to form entry
+        </NavLink>
         <Switch>
-          <Route path="/" component={Dashboard} />
+          <Route exact path="/" component={Dashboard} />
           <Route path="/FormEntry" component={FormEntry} />
           <Route component={Error} />
         </Switch>
