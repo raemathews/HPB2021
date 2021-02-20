86682cdbbc045ffff051e8b51f59ae85cf9a3f48
raemathews <mathews.ra@northeastern.edu>
2021-02-20 00:52:29 -0500

    add Dashboard component

---
+++ vaccine-tracker-frontend/src/Dashboard.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Dashboard extends React.Component {
+
+    constructor() {
+
+    }
+    
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    }
+}
