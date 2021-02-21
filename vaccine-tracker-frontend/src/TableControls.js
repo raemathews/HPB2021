32036cbc76b8f898b2e6a92c7a9d93dab73fb770
raemathews <mathews.ra@northeastern.edu>
2021-02-20 23:26:09 -0500

    outdated table controls

--- vaccine-tracker-frontend/src/TableControls.js
+++ vaccine-tracker-frontend/src/TableControls.js
@@ -5,11 +5,13 @@
 export default function TableControls() {
     return (
         <div>
-            <input type="radio" id="bottom" value="bottom" name="b1"></input>
-            <label for="bottom">10 Least Wasteful Counties</label>
-            <br/>
-            <input type="radio" id="top" value="top" name="b1"></input>
-            <label for="top">10 Most Wasteful Counties</label>
+            <label class="dataselection" for="tablevalues">View:</label>
+            <select className="form-control mb-3" onChange={(e) => this.setState({ tableDataDescription: e.target.value})}>
+                <option value="topten">10 Most Wasteful Counties</option>
+                <option value="bottomten">10 Least Wasteful Counties</option>
+            </select>
         </div>
     )
 }
+
+
